49399339c99927488a64f005ad24c422550a44a8
katabame <katabamia@gmail.com>
2018-07-30 10:30:33 +0900

    Add: activate, deactivate for venv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,22 @@ set -g theme_color_scheme terminal2
 export SCREENDIR=$HOME/.screen
 
 function fish_greeting
-	screenfetch
+	#screenfetch
 end
 
 function cd
 	builtin cd $argv
 	ls -a
+end
+
+function activate
+	set -lx PATH ./bin $PATH
+	echo -n You are now enter venv : ; which pip
+end
+
+function deactivate
+	set -lx PATH $PATH[2..-1]
+	echo -n You are now leave venv : ; which pip
 end
 
 alias rm='rm -i'
